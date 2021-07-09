65da0727ac35cdb8dfc38b9ea7e8b0673d74279f
ikonst87 <ikonstantinovskyi@bostonsd.ru>
2021-07-09 17:15:03 +0300

    Project 1 main task

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+
+@app.route("/departures/<departure>/")
+def render_departure(departure):
+    return render_template('departure.html')
+
+
+@app.route("/tours/<id>/")
+def render_tours(tour_id):
+    return render_template('tour.html')
+
+
+if __name__ == '__main__':
+    app.run()
