de1f45208621e966140943dab308da71600753d8
ikonst87 <ikonstantinovskyi@bostonsd.ru>
2021-07-10 14:45:50 +0300

    Выполнил дополнительное задание

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import data
+
 
 app = Flask(__name__)
 
@@ -14,8 +16,31 @@ def render_departure(departure):
 
 
 @app.route("/tours/<id>/")
-def render_tours(tour_id):
+def render_tours(id):
     return render_template('tour.html')
+
+
+@app.route("/data/")
+def main_data():
+    print(data.tours[1]["country"])
+    return render_template('data.html', tours=data.tours)
+
+
+@app.route("/data/departures/<departure>/")
+def data_departure(departure):
+    return render_template('data_departures.html', tours=data.tours)
+
+
+@app.route("/data/tours/<id>/")
+def data_tours(id):
+    tour_id = int(id)
+    country = data.tours[tour_id]["country"]
+    hotel = data.tours[tour_id]["title"]
+    price = data.tours[tour_id]["price"]
+    duration = data.tours[tour_id]["nights"]
+    description = data.tours[tour_id]["description"]
+    print(country)
+    return render_template('data_tours.html', country=country, hotel=hotel, price=price, duration=duration, description=description)
 
 
 if __name__ == '__main__':
